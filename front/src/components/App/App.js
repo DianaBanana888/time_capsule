da5075f8bc392a091eeea6d89c50b9bad8de00d9
Maria Murashova <maria_murashova@mail.ru>
2021-03-05 12:44:43 +0300

    пятница Мария 12:44

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,27 +1,31 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Main from '../Main';
-import './App.css';
-import Navbar from '../Navbar';
-import Home from '../Home';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "../Main";
+import "./App.css";
+import Navbar from "../Navbar";
+import Home from "../Home";
+import Drawer from "../Drawer";
 
-import LetterForm from '../../pages/LetterForm';
+import LetterForm from "../../pages/LetterForm";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="App ">
-        <Main />
-      </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/create">
-          <LetterForm />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Drawer />
+        {/* <div className="App ">
+          <Main />
+        </div> */}
+        <Switch>
+          {/* <Route exact path="/">
+            <Home />
+          </Route> */}
+
+          <Route exact path="/create">
+            <LetterForm />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ front/src/components/Drawer.js
@@ -0,0 +1,118 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import HomeIcon from "@material-ui/icons/Home";
+import MailIcon from "@material-ui/icons/Mail";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Faq from "./Faq";
+import LetterForm from "../pages/LetterForm";
+import Auth from "../pages/Auth";
+
+import LogOut from "./LogOut";
+
+import { useSelector } from "react-redux";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonForce() {
+  const { isAuth } = useSelector((state) => state);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="on"
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="scrollable force tabs example"
+        >
+          <Tab label="Home" icon={<HomeIcon />} {...a11yProps(0)} />
+          {isAuth ? (
+            <Tab label="LogOut" icon={<PersonPinIcon />} {...a11yProps(1)} />
+          ) : (
+            <Tab label="LogIn" icon={<PersonPinIcon />} {...a11yProps(1)} />
+          )}
+
+          <Tab label="Mail" icon={<MailIcon />} {...a11yProps(2)} />
+          <Tab label="FAQ" icon={<HelpIcon />} {...a11yProps(3)} />
+          <Tab label="Account" icon={<ShoppingBasket />} {...a11yProps(4)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Home />
+      </TabPanel>
+
+      <TabPanel value={value} index={1}>
+        {isAuth ? <LogOut /> : <Auth />}
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <LetterForm />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <Faq />
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Four
+      </TabPanel>
+    </div>
+  );
+}
