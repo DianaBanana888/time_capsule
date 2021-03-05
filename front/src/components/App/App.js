35b1ec7320996a61347c32dc3f67ea99de433e19
Maria Murashova <maria_murashova@mail.ru>
2021-03-05 18:19:00 +0300

    пятница Мария 18:18

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -7,24 +7,33 @@ import Drawer from "../Drawer";
 
 import LetterForm from "../../pages/LetterForm";
 
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+// pick a date util library
+import MomentUtils from "@date-io/moment";
+import DateFnsUtils from "@date-io/date-fns";
+import LuxonUtils from "@date-io/luxon";
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Drawer />
-        {/* <div className="App ">
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <BrowserRouter>
+          <Drawer />
+          {/* <div className="App ">
           <Main />
         </div> */}
-        <Switch>
-          {/* <Route exact path="/">
+          <Switch>
+            {/* <Route exact path="/">
             <Home />
           </Route> */}
 
-          <Route exact path="/create">
-            <LetterForm />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+            <Route exact path="/create">
+              <LetterForm />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </MuiPickersUtilsProvider>
     </>
   );
 }
