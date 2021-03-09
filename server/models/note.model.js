99934dad1879d24475e5dd1706638a5eab0109ba
Maria Murashova <maria_murashova@mail.ru>
2021-03-09 09:39:32 +0300

    стили

--- server/models/note.model.js
+++ server/models/note.model.js
@@ -1,13 +1,16 @@
-const { Schema, model } = require('mongoose');
-
+const { Schema, model } = require("mongoose");
+const addDays = 3;
+const date = new Date();
 const NoteModelSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: 'UserModel' },
+  author: { type: Schema.Types.ObjectId, ref: "UserModel" },
   text: { type: String },
   photo: { type: String },
   video: { type: String },
   private: { type: Boolean, default: true },
   receivers: { type: String },
-  deliveryDate: { type: Date },
+  // deliveryDate: { type: Date },
+  sendDate: { type: Date, default: date.setDate(date.getDate() + addDays) },
+  time: { type: String },
 });
 
-module.exports = model('NoteModel', NoteModelSchema);
+module.exports = model("NoteModel", NoteModelSchema);
